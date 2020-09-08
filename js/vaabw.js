5bdafb17895142ff5a30be418b8702924f9ba1a5
thomas15v <tvanmellaerts@gmail.com>
2020-09-08 10:35:37 +0200

    Add VAAOG

---
+++ js/vaabw.js
@@ -0,0 +1,50 @@
+let cadastralValue;
+
+const yearConst = {
+    2020: {
+        index: 1.8230,
+        heating: 2030.00,
+        electric: 1010
+    },
+    2021: {
+        index: 1.8492,
+        heating: 2060.00,
+        electric: 1030
+    }
+}
+
+
+function calculateBenefits(cadastralValue, year, furnished){
+    console.log((furnished ?  5/3 : 1))
+    return cadastralValue * yearConst[year]['index'] * (100 / 60) * 2 * (furnished ?  5/3 : 1)
+}
+
+function calculate(){
+    const cadastral = parseFloat(cadastralValue.get());
+    const year = $('#year').val();
+    const furnished = $('#furnished').prop('checked')
+    const heating = $('#heating').prop('checked');
+    const electricity = $('#electricity').prop('checked')
+
+    $('#total').text(AutoNumeric.format(calculateBenefits(cadastral, year, furnished), AutoNumeric.getPredefinedOptions().French));
+
+    if (heating){
+        $('#totalHeating').text(AutoNumeric.format(yearConst[year]['heating'], AutoNumeric.getPredefinedOptions().French));
+        $('#totalHeatingDiv').removeClass('hiddendiv');
+    } else {
+        $('#totalHeatingDiv').addClass('hiddendiv');
+    }
+
+    if (electricity) {
+        $('#totalElectricity').text(AutoNumeric.format(yearConst[year]['electric'], AutoNumeric.getPredefinedOptions().French));
+        $('#totalElectricityDiv').removeClass('hiddendiv');
+    } else {
+        $('#totalElectricityDiv').addClass('hiddendiv');
+    }
+}
+
+$(document).ready(function () {
+    cadastralValue = new AutoNumeric('#cadastral', 'euroPos');
+    $('form :input').change(calculate);
+    calculate(cadastralValue)
+})
--- js/website.js
+++ js/website.js
@@ -1,5 +1,5 @@
+const months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']
 $(document).ready(function(){
-    const months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('.tooltipped').tooltip();
@@ -18,51 +18,5 @@ $(document).ready(function(){
             weekdaysShort: ['Zon', 'Maa', 'Din', 'Woe', 'Don', 'Vri', 'Zat'],
             weekdaysAbbrev: ['Z', 'M', 'D', 'W', 'D', 'V', 'Z']
         },
-    });
-    const inputCatalogValue = new AutoNumeric('#catalogValue', 'euroPos');
-    const inputco2 = new AutoNumeric('#co2', options = {
-        maximumValue: 999,
-        minimumValue: 0,
-        decimalPlaces: 0,
-        suffixText: ' gr/km'
-    });
-    $('form :input').change(function(){
-        const catalogValue = parseFloat(inputCatalogValue.get());
-
-        const fuelType = $('#fuelType').val();
-        let co2 = parseInt(inputco2.get());
-        let firstRegistrationDate = $('#firstRegistrationDate').val();
-        const year = parseInt($('#year').val());
-
-        if (fuelType && fuelType == 'electric'){
-            co2 = 0;
-            $('#co2Field').addClass('hiddendiv');
-        } else {
-            $('#co2Field').removeClass('hiddendiv');
-        }
-
-        if (catalogValue && fuelType && Number.isInteger(co2) && firstRegistrationDate && year){
-            $("#data").removeClass('hiddendiv');
-            $('#tabledata').empty();
-            const table = $('#tabledata');
-            const dateArray = firstRegistrationDate.split('/')
-            firstRegistrationDate = new Date(dateArray[2] + '-' + dateArray[1] + '-' + dateArray[0])
-            let total = 0;
-            for (let i = 1; i <= 12; i++){
-                let month = months[i-1]
-                let value = calculateBenefitsForMonth(i, year, catalogValue, fuelType, co2, firstRegistrationDate)
-                total += value;
-                table.append("<tr><td>" + month + "</td><td>" + AutoNumeric.format(value, AutoNumeric.getPredefinedOptions().French) + "</td></tr>")
-            }
-            $('#totalfield').text(AutoNumeric.format(total, AutoNumeric.getPredefinedOptions().French))
-
-            //aftrekbaarheid addon
-            $('#fuelCoefficientDiv').removeClass('hiddendiv');
-            let fuelCoefficient = calculateDeductible(year, fuelType, co2)
-            $('#fuelCoefficient').html("Fiscaal aftrekbaar : " + fuelCoefficient + " %");
-        } else {
-            $("#data").addClass('hiddendiv');
-            $('#tabledata').empty()
-        }
     });
 });
